d7c536d68555fa90813cbb69b8df0ae40fe07112
moonbreezee <1014913222@qq.com>
2017-09-01 17:12:48 +0800

    add

--- 前端数据模拟/server.js
+++ 前端数据模拟/server.js
@@ -4,7 +4,7 @@ var express = require('express');
 var path = require('path');
 var opn = require('opn');
 // 用于在服务端
-var fetchMock = require('fetch-mock');
+var FetchMock = require('fetch-mock');
 var Mock = require('mockjs');
 // .setup({timeout: '3000'}) 一直提示不是一个函数
 var app = express();
@@ -18,10 +18,23 @@ app.get('/', function (req, res) {
 
 // 拦截fetch，mock, 可以
 app.get('/api/fetch', function (req, res){
+	// 同样
 	setTimeout(function() {
 		res.send(JSON.stringify(Mock.mock({'hello': '@cname'}),null, 4));
 	}, 3000);	
 });
+
+// 拦截fetch，mock, 可以
+app.get('/api/fetch-2', function (req, res){
+	// 同样
+	var data = Mock.mock({
+		'fetch-2': '@cname'
+	})
+	var jsonObj = JSON.stringify(data, null, 4);
+	console.log("jsonObj: ", jsonObj);
+	res.send(jsonObj);
+});
+
 
 var server = app.listen(3000, function () {
 	var host = server.address().address;
