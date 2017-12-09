2058ec16fa975e083a31bf6fbc25486bc0fdc65c
moonbreezee <1014913222@qq.com>
2017-12-09 15:23:39 +0800

    aaa

--- logo.js
+++ logo.js
@@ -83,3 +83,6 @@ console.log('程序员A：哎 太累了日子没法过了 怎么才能换行啊'
 console.log('程序员B：打回车呀！');
 
 console.log('%c愿世界和平，永无BUG！', 'background-image:-webkit-gradient( linear, left top, right top, color-stop(0, #f22), color-stop(0.15, #f2f), color-stop(0.3, #22f), color-stop(0.45, #2ff), color-stop(0.6, #2f2),color-stop(0.75, #2f2), color-stop(0.9, #ff2), color-stop(1, #f22) );color:transparent;-webkit-background-clip: text;font-size:2em;');
+
+
+
