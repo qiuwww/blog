4ffbc3d400daac23e86b4da3e9fcb8dd66cd2965
moonbreezee <qw911029>
2017-12-10 22:53:47 +0800

    add

---
+++ JS/Utils.js
@@ -0,0 +1,18 @@
+
+// 整个语句都开启严格模式的语法
+"use strict";
+// 总库，需要的复制出来，不至于每次都占用过多内存
+export default Utils = {
+	isInteger(x) { 
+		return (x^0) === x; 
+	},
+	log(msg) {
+		console.log(msg)
+	}	
+}
+
+
+
+
+
+
