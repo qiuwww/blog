2514cda0365ef771c3fc5eb201fc62c3ef878eea
qiuwww <1014913222@qq.com>
2023-10-11 00:45:58 +0800

    update

--- 9.前端工程化/webpack/webpack-5/scripts/loader-modify.js
+++ 9.前端工程化/webpack/webpack-5/scripts/loader-modify.js
@@ -8,14 +8,17 @@ module.exports = function loader(source) {
   if (this.resourcePath.endsWith('ast.js')) {
     console.log('source path: ', this.resourcePath);
 
-    return `export default ${JSON.stringify(source)} \n console.log("loader-modify add")`;
+    // return `export default ${JSON.stringify(source)} \n console.log("loader-modify add")`;
 
     // !这里也可以进行ast分析，然后修改代码
     // !这里的修改只会被执行一次
     // source = source.replace(/\[name\]/g, options.name);
+
+    return `${source} \n console.log("loader-modify add")`;
   }
 
   // 在目标文件后边追加内容
+  // return `export default ${JSON.stringify(source)}`;
 
-  return `export default ${JSON.stringify(source)}`;
+  return source;
 };
