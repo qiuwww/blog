5ae6024ba354ea58df8caebb4848a27dddac4cce
qiuwww <1014913222@qq.com>
2024-11-20 16:50:33 +0800

    update

--- 15.运维操作及环境配置/docker/docker-demo2-realtime/index.js
+++ 15.运维操作及环境配置/docker/docker-demo2-realtime/index.js
@@ -2,8 +2,8 @@ const Koa = require('koa');
 const app = new Koa();
 
 app.use(async (ctx) => {
-  console.log('!!!');
-  ctx.body = 'Hello World!!';
+  console.log('!!!!!!');
+  ctx.body = 'Hello World!!!!!';
 });
 
 app.listen(3000);
